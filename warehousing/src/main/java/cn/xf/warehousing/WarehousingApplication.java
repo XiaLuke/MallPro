2036fcf26d0fa2ad8c2f2cd3805ab6dc04984a21
xiafan <xiafan@skyvis.com.cn>
2022-02-04 20:39:12 +0800

    初始化创建

---
+++ warehousing/src/main/java/cn/xf/warehousing/WarehousingApplication.java
@@ -0,0 +1,13 @@
+package cn.xf.warehousing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehousingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehousingApplication.class, args);
+    }
+
+}
